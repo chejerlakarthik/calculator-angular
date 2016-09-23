714746f8ab3755483570b5c930da89d5a837962b
Chejerla <karthik.chejerla@honeywell.com>
2016-09-23 21:42:52 +0530

    added closures

--- js/controllers/calculator-controller.js
+++ js/controllers/calculator-controller.js
@@ -1,75 +1,82 @@
-angular.module('calculatorApp').controller('calculatorCtrl', function ($scope, calculatorService) {
-    $scope.operations = calculatorService.getAllOperations();
-    $scope.singleOperandOperations = calculatorService.getSingleOperandOperations();
-    $scope.error = '';
-    $scope.selOper = $scope.operations[0];
-    $scope.showDetails = false;
-    // Function that executes when an operation is selected from drop-down
-    $scope.changeSelection = function (operation) {
-        $scope.selOper = operation;
-        if ($scope.oneOperandOper()) {
-            $scope.operandOneLabel = 'Input Value :';
-        }
-        else {
-            $scope.operandOneLabel = 'Operand One :';
-        }
-    };
-    // Function that executes on clicking Submit button
-    $scope.performOperation = function () {
+(function () {
+    'use strict';
+    angular.module('calculatorApp').controller('calculatorCtrl', function ($scope, $interval, calculatorService) {
+        $scope.operations = calculatorService.getAllOperations();
+        $scope.singleOperandOperations = calculatorService.getSingleOperandOperations();
         $scope.error = '';
-        if ($scope.selOper === $scope.operations[0]) {
-            $scope.error = 'Please select an operation first!';
-        }
-        else if ($scope.operandOne === '' || $scope.operandTwo === '') {
-            $scope.error = 'Invalid Inputs';
-            if (!($scope.oneOperandOper() && $scope.operandOne === '')) {
-                $scope.error = '';
-            }
-        }
-        else if (($scope.operandOne == '' || $scope.operandTwo == '') && !$scope.oneOperandOper()) {
-            $scope.error = 'Invalid inputs';
-        }
-        if ($scope.selOper !== $scope.operations[0]) {
-            var first = parseFloat($scope.operandOne);
-            var second = parseFloat($scope.operandTwo);
-            var operands = [first, second];
-            var response = calculatorService.executeOperation($scope.selOper)(operands);
-            if (response.status === 0) {
-                $scope.result = response.result;
-                $scope.resultLabel = response.resultLabel;
-                if (first === second !== '' && $scope.selOper === $scope.operations[3]) {
-                    $scope.message = 'You should try using SQUARE operation';
-                }
+        $scope.selOper = $scope.operations[0];
+        $scope.showDetails = false;
+        // Function that executes when an operation is selected from drop-down
+        $scope.changeSelection = function (operation) {
+            $scope.selOper = operation;
+            if ($scope.oneOperandOper()) {
+                $scope.operandOneLabel = 'Input Value :';
             }
             else {
-                $scope.error = response.message;
+                $scope.operandOneLabel = 'Operand One :';
             }
-        }
-    };
-    // Form reset
-    $scope.clear = function () {
+        };
+        // Function that executes on clicking Submit button
+        $scope.performOperation = function () {
             $scope.error = '';
-            $scope.operandOne = '';
-            $scope.operandTwo = '';
-            $scope.selOper = $scope.operations[0];
-            $scope.result = '';
-            $scope.operandOneLabel = 'Operand One :';
-            $scope.resultLabel = 'Result :';
-            $scope.message = '';
+            if ($scope.selOper === $scope.operations[0]) {
+                $scope.error = 'Please select an operation first!';
+            }
+            else if ($scope.operandOne === '' || $scope.operandTwo === '') {
+                $scope.error = 'Invalid Inputs';
+                if (!($scope.oneOperandOper() && $scope.operandOne === '')) {
+                    $scope.error = '';
+                }
+            }
+            else if (($scope.operandOne == '' || $scope.operandTwo == '') && !$scope.oneOperandOper()) {
+                $scope.error = 'Invalid inputs';
+            }
+            if ($scope.selOper !== $scope.operations[0]) {
+                var first = parseFloat($scope.operandOne);
+                var second = parseFloat($scope.operandTwo);
+                var operands = [first, second];
+                var response = calculatorService.executeOperation($scope.selOper)(operands);
+                if (response.status === 0) {
+                    $scope.result = response.result;
+                    $scope.resultLabel = response.resultLabel;
+                    if (first === second !== '' && $scope.selOper === $scope.operations[3]) {
+                        $scope.message = 'You should try using SQUARE operation';
+                    }
+                }
+                else {
+                    $scope.error = response.message;
+                }
+            }
+        };
+        // Form reset
+        $scope.clear = function () {
+                $scope.error = '';
+                $scope.operandOne = '';
+                $scope.operandTwo = '';
+                $scope.selOper = $scope.operations[0];
+                $scope.result = '';
+                $scope.operandOneLabel = 'Operand One :';
+                $scope.resultLabel = 'Result :';
+                $scope.message = '';
+            }
+            // Hide second operand field
+        $scope.oneOperandOper = function () {
+                return ($scope.singleOperandOperations.indexOf($scope.selOper) != -1);
+            }
+            // Do not allow non-numeric characters as inputs. .(dot) is allowed.
+        $(document).ready(function () {
+            $('.number').numeric;
+        });
+        $scope.toggleDetails = function () {
+            console.log('Toggling Show details');
+            return !$scope.showDetails;
         }
-        // Hide second operand field
-    $scope.oneOperandOper = function () {
-            return ($scope.singleOperandOperations.indexOf($scope.selOper) != -1);
+        $scope.doubleClicked = function () {
+            window.alert('Single click is sufficient to reset the page');
         }
-        // Do not allow non-numeric characters as inputs. .(dot) is allowed.
-    $(document).ready(function () {
-        $('.number').numeric;
+        $scope.currentTime = new Date().toLocaleTimeString();
+        $interval(function () {
+            $scope.currentTime = new Date().toLocaleTimeString();
+        }, 1000);
     });
-    $scope.toggleDetails = function () {
-        console.log('Toggling Show details');
-        return !$scope.showDetails;
-    }
-    $scope.doubleClicked = function () {
-        window.alert('Single click is sufficient to reset the page');
-    }
-});
+})();
--- js/controllers/login-controller.js
+++ js/controllers/login-controller.js
@@ -1,16 +1,19 @@
-angular.module('calculatorApp').controller('loginCtrl', loginCtrl);
+(function () {
+    'use strict';
+    angular.module('calculatorApp').controller('loginCtrl', loginCtrl);
 
-function loginCtrl($scope, $location) {
-    $scope.isAuthenticated = false;
-    $scope.login = function () {
-        if ($scope.user.name === 'admin' && $scope.user.pwd === 'password') {
-            $scope.isAuthenticated = true;
-            $location.path('/calculator');
-        }
-        else {
-            $scope.user = {};
-            $location.path('/');
-            console.log('Authentication failed');
+    function loginCtrl($scope, $location) {
+        $scope.isAuthenticated = false;
+        $scope.login = function () {
+            if ($scope.user.name === 'admin' && $scope.user.pwd === 'password') {
+                $scope.isAuthenticated = true;
+                $location.path('/calculator');
+            }
+            else {
+                $scope.user = {};
+                $location.path('/');
+                console.log('Authentication failed');
+            }
         }
     }
-}
+})();
